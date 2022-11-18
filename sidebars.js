90de6f57f64a3ed0bad42edcf0c2ec788075ae6c
Alexander Wang <alex@terrastruct.com>
2022-11-17 16:28:38 -0800

    2022-11-17 04:28:38PM

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,6 @@ const sidebars = {
       type: "category",
       label: "Introduction",
       link: { type: "doc", id: "tour/intro" },
-      collapsed: true,
       items: [
         "tour/intro",
         "tour/experience",
@@ -17,7 +16,6 @@ const sidebars = {
       type: "category",
       label: "Getting Started",
       link: { type: "doc", id: "tour/install" },
-      collapsed: true,
       items: [
         "tour/install",
         "tour/hello-world",
@@ -30,28 +28,24 @@ const sidebars = {
     {
       type: "category",
       label: "More Objects",
-      collapsed: true,
       link: { type: "doc", id: "tour/text" },
       items: ["tour/text", "tour/icons", "tour/sql-tables", "tour/classes"],
     },
     {
       type: "category",
       label: "Customization",
-      collapsed: true,
       link: { type: "doc", id: "tour/layouts" },
       items: ["tour/layouts", "tour/themes", "tour/style"],
     },
     {
       type: "category",
       label: "In Depth",
-      collapsed: true,
       link: { type: "doc", id: "tour/strings" },
       items: ["tour/strings", "tour/comments", "tour/overrides", "tour/auto-formatter"],
     },
     {
       type: "category",
       label: "Extensions",
-      collapsed: true,
       link: { type: "doc", id: "tour/extensions" },
       items: ["tour/vscode", "tour/vim"],
     },
