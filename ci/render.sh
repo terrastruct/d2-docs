81c1856b9e7b84649ad63122f74138c7583661b7
Alexander Wang <alex@terrastruct.com>
2022-12-25 17:27:30 -0800

    update near

--- ci/render.sh
+++ ci/render.sh
@@ -5,5 +5,5 @@ do
  echo "Processing $f"
  filename=$(basename -- "$f")
  filename="${filename%.*}"
- D2_LAYOUT=dagre d2 $f ./static/img/generated/${filename}.svg2
+ d2 -l dagre --pad 0 $f ./static/img/generated/${filename}.svg2
 done
