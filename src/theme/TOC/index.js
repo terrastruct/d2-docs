03f8a48a7a54023ce6d788aad8cd52dcc3927506
Bernard Xie <bernard@terrastruct.com>
2023-04-03 15:50:48 -0700

    custom table of contents

---
+++ src/theme/TOC/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import clsx from "clsx";
+import TOCItems from "@theme/TOCItems";
+import styles from "./styles.module.css";
+// Using a custom className
+// This prevents TOCInline/TOCCollapsible getting highlighted by mistake
+const LINK_CLASS_NAME = "table-of-contents__link toc-highlight";
+const LINK_ACTIVE_CLASS_NAME = "table-of-contents__link--active";
+export default function TOC({ className, ...props }) {
+  return (
+    <div className={clsx(styles.tableOfContents, "thin-scrollbar", className)}>
+      <div className={styles.tableOfContentsHeader}>Contents</div>
+      <TOCItems
+        {...props}
+        linkClassName={LINK_CLASS_NAME}
+        linkActiveClassName={LINK_ACTIVE_CLASS_NAME}
+      />
+    </div>
+  );
+}
