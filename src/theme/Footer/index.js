ed93e74e7a2346c96df4fcb375827185ee0e7d0c
Bernard Xie <bernard@terrastruct.com>
2023-04-10 14:13:41 -0700

    new footer style

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -15,7 +15,16 @@ function Footer() {
   const playground = rightItems.find((i) => i.label === "Playground");
   return (
     <footer className="Footer">
-      <img className="Footer__Logo" src="/img/d2_logo.png" />
+      <div className="Footer--Top">
+        <img className="Footer__Logo" src="/img/d2_logo.png" />
+        <div className="Footer__Mobile">
+          <div className="Footer__Mobile--Icons">
+            {icons.map((item, i) => (
+              <NavbarItem key={i} {...item} />
+            ))}
+          </div>
+        </div>
+      </div>
       <div className="Footer__Right">
         <a className="Footer__Link" href="https://terrastruct.com">
           <span>
@@ -26,19 +35,12 @@ function Footer() {
       </div>
       <div className="Footer__Mobile">
         <div className="Footer__Mobile--Bar">
-          <div className="Footer__Mobile--Top">
+          <div className="Footer__Mobile--Bottom">
             <div className="Footer__Mobile--Links">
               {links.map((item, i) => (
                 <NavbarItem key={i} {...item} />
               ))}
             </div>
-            <div className="Footer__Mobile--Icons">
-              {icons.map((item, i) => (
-                <NavbarItem key={i} {...item} />
-              ))}
-            </div>
-          </div>
-          <div className="Footer__Mobile--Bottom">
             <NavbarItem {...playground} className="Footer__Mobile--Playground" />
           </div>
         </div>
