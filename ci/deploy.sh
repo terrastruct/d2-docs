0559a65e4c8103b5bf0179fc4c52124b4b8fa9f5
Alexander Wang <alex@terrastruct.com>
2023-04-27 13:42:22 -0700

    [ci-force] trigger algolia crawl on deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -11,7 +11,18 @@ if [ -z ${D2_DOCS_S3_BUCKET} ]; then
   exit 1
 fi
 
+if [ -z ${D2_DOCS_ALGOLIA_CRAWLER_API_KEY} ]; then
+  echo "missing env var D2_DOCS_ALGOLIA_CRAWLER_API_KEY"
+  exit 1
+fi
+
 npm run prod
 aws sts get-caller-identity
 aws s3 sync ./build ${D2_DOCS_S3_BUCKET} --delete --acl public-read
 aws cloudfront create-invalidation --distribution-id ${D2_DOCS_CLOUDFRONT_ID} --paths "/*"
+
+curl -H "Content-Type: application/json" \
+  -X POST \
+  --user dd0bd3fe-db42-4673-89ae-e7e70dfae16b:${D2_DOCS_ALGOLIA_CRAWLER_API_KEY} \
+  "https://crawler.algolia.com/api/1/crawlers/2d841c67-ab75-4fb1-9114-f8844363d74c/reindex"
+
