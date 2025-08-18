c7b5dcd2a7b89ea993a3361fb9a229c9ce88d832
Alexander Wang <alex@terrastruct.com>
2025-08-18 14:29:33 -0600

    re-render omitting version

--- static/blog/sketch/render.sh
+++ static/blog/sketch/render.sh
@@ -1,5 +1,5 @@
 cd $(git rev-parse --show-toplevel)
 
-d2 -l elk --pad 20 --sketch ./static/blog/sketch/main.d2 ./static/blog/sketch/main.svg2
+d2 --omit-version -l elk --pad 20 --sketch ./static/blog/sketch/main.d2 ./static/blog/sketch/main.svg2
 
-d2 --pad 20 --sketch ./static/blog/sketch/animated.d2 ./static/blog/sketch/animated.svg2
+d2 --omit-version --pad 20 --sketch ./static/blog/sketch/animated.d2 ./static/blog/sketch/animated.svg2
